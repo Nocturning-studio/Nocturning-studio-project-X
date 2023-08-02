85445eb1da3236eac601193b36a6fa9c5cf28215
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2023-08-02 11:53:44 +0400

    Перенес код фоновой музыки и статичных звуков от чистого неба, чтобы использовать стерео одним звуковым файлом и воспроизводить музыку и ночью

--- xrGame/level_sounds.h
+++ xrGame/level_sounds.h
@@ -15,8 +15,8 @@ struct SStaticSound
 	float			m_Volume;
 	float			m_Freq;
 public:
-	void 			Load			(IReader& F);
-	void 			Update			(u32 gt, u32 rt);
+	void 			Load(IReader& F);
+	void 			Update(u32 gt, u32 rt);
 };
 
 // music interface
@@ -25,32 +25,34 @@ struct	SMusicTrack
 #ifdef DEBUG
 	shared_str		m_DbgName;
 #endif
-	ref_sound		m_SourceLeft;
-	ref_sound		m_SourceRight;
+	ref_sound		m_SourceStereo;
 	Ivector2		m_ActiveTime;
 	Ivector2		m_PauseTime;
 	float			m_Volume;
 public:
-	void			Load			(LPCSTR fn, LPCSTR params);
-	BOOL			IsPlaying		(){return m_SourceLeft._feedback() || m_SourceRight._feedback();}
-	void			Play			();
-	void			Stop			();
-	void			SetVolume		(float volume);
+	void			Load(LPCSTR fn, LPCSTR params);
+	BOOL			in(u32 game_time);
+	BOOL			IsPlaying();
+	void			Play();
+	void			Stop();
+	void			SetVolume(float volume);
 };
 
 class CLevelSoundManager
 {
-	DEFINE_VECTOR(SStaticSound,StaticSoundsVec,StaticSoundsVecIt);
+	using StaticSoundsVec = xr_vector<SStaticSound>;
+	using StaticSoundsVecIt = StaticSoundsVec::iterator;
 	StaticSoundsVec	m_StaticSounds;
-	DEFINE_VECTOR(SMusicTrack,MusicTrackVec,MusicTrackVecIt);
+	using MusicTrackVec = xr_vector<SMusicTrack>;
+	using MusicTrackVecIt = MusicTrackVec::iterator;
 	MusicTrackVec	m_MusicTracks;
 	u32				m_NextTrackTime;
 	int				m_CurrentTrack;
 public:
-					CLevelSoundManager();
-	void			Load			();
-	void			Unload			();
-	void __stdcall	Update			();
+	CLevelSoundManager();
+	void			Load();
+	void			Unload();
+	void __stdcall	Update();
 };
 
 #endif
