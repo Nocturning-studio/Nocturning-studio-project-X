f82b684969639ee64f8af14d784e416a320eacac
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2023-08-02 11:54:12 +0400

    Исправил глупую ошибку с громкостями

--- xrSound/SoundRender_Emitter_FSM.cpp
+++ xrSound/SoundRender_Emitter_FSM.cpp
@@ -56,7 +56,7 @@ void CSoundRender_Emitter::update(float dt)
 		fTimeToPropagade = fTime;
 		fade_volume = 1.f;
 		occluder_volume = SoundRender->get_occlusion(p_source.position, .2f, occluder);
-		smooth_volume = p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects * psSoundVFactor : psSoundVEffects * psSoundVMusic) * (b2D ? 1.f : occluder_volume);
+		smooth_volume = p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects : psSoundVMusic) * psSoundVFactor * (b2D ? 1.f : occluder_volume);
 		e_current = e_target = *SoundRender->get_environment(p_source.position);
 
 		if (update_culling(dt))
@@ -86,7 +86,7 @@ void CSoundRender_Emitter::update(float dt)
 		fTimeToPropagade = fTime;
 		fade_volume = 1.f;
 		occluder_volume = SoundRender->get_occlusion(p_source.position, .2f, occluder);
-		smooth_volume = p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects * psSoundVFactor : psSoundVEffects * psSoundVMusic) * (b2D ? 1.f : occluder_volume);
+		smooth_volume = p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects : psSoundVMusic) * psSoundVFactor * (b2D ? 1.f : occluder_volume);
 		e_current = e_target = *SoundRender->get_environment(p_source.position);
 
 		if (update_culling(dt))
@@ -265,9 +265,9 @@ BOOL CSoundRender_Emitter::update_culling(float dt)
 		// Calc attenuated volume
 		float att = p_source.min_distance / (psSoundRolloff * dist);
 		clamp(att, 0.f, 1.f);
-		float fade_scale = bStopping || (att * p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects * psSoundVFactor : psSoundVEffects * psSoundVMusic) < psSoundCull) ? -1.f : 1.f;
+		float fade_scale = bStopping || (att * p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects * psSoundVFactor : psSoundVMusic * psSoundVFactor) < psSoundCull) ? -1.f : 1.f;
 
-/*
+
 		if (owner_data->g_type == SOUND_TYPE_WEAPON ||
 			owner_data->g_type == SOUND_TYPE_SHOOTING ||
 			owner_data->g_type == SOUND_TYPE_EMPTY_CLICKING ||
@@ -278,7 +278,7 @@ BOOL CSoundRender_Emitter::update_culling(float dt)
 			owner_data->g_type == SOUND_TYPE_WEAPON_BULLET_HIT ||
 			owner_data->g_type == SOUND_TYPE_WEAPON_RECHARGING)
 			fade_scale *= psSoundVWeaponShooting;
-*/
+
 		fade_volume += dt * 10.f * fade_scale;
 
 		// Update occlusion
@@ -289,7 +289,7 @@ BOOL CSoundRender_Emitter::update_culling(float dt)
 	}
 	clamp(fade_volume, 0.f, 1.f);
 	// Update smoothing
-	smooth_volume = .9f * smooth_volume + .1f * (p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects * psSoundVFactor : psSoundVEffects * psSoundVMusic) * occluder_volume * fade_volume);
+	smooth_volume = .9f * smooth_volume + .1f * (p_source.base_volume * p_source.volume * (owner_data->s_type == st_Effect ? psSoundVEffects : psSoundVMusic) * psSoundVFactor * occluder_volume * fade_volume);
 
 	if (owner_data->g_type == SOUND_TYPE_WEAPON ||
 		owner_data->g_type == SOUND_TYPE_SHOOTING ||
