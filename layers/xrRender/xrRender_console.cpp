2f3bdab86032aa0e743aa835c7b2143f9b132be8
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2023-08-04 01:27:41 +0400

    Теперь глубина резкости влияет и на небо (см. dof.h)

--- layers/xrRender/xrRender_console.cpp
+++ layers/xrRender/xrRender_console.cpp
@@ -277,7 +277,7 @@ float		ps_r2_slight_fade = 1.f;				// 1.f
 
 //	x - min (0), y - focus (1.4), z - max (100)
 Fvector3	ps_r2_dof = Fvector3().set(-1.25f, 1.4f, 600.f);
-float		ps_r2_dof_sky = 30;				//	distance to sky
+float		ps_r2_dof_sky = 300;				//	distance to sky
 float		ps_r2_dof_kernel_size = 5.0f;						//	7.0f
 
 
