e718eb888463d40522f93068332db813cffa2ff5
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2023-10-08 23:19:26 +0400

    Перенос фиксов: Фикс IXRAY "Fix imspossible head rotation of trader"

--- xrGame/ai/trader/ai_trader.cpp
+++ xrGame/ai/trader/ai_trader.cpp
@@ -103,6 +103,7 @@ void CAI_Trader::LookAtActor(CBoneInstance *B)
 	XFORM().getHPB(h,p,b);
 	float cur_yaw	= h;
 	float dy		= _abs(angle_normalize_signed(yaw - cur_yaw));
+	clamp(dy, 0.f, 1.f); //FIX BY IXRAY(THANKS BY DEATHMAN)
 
 	if (angle_normalize_signed(yaw - cur_yaw) > 0) dy *= -1.f;
 
