188a3345c21646b2bd30f3e0e4348568e76af0a0
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2023-11-27 21:13:41 +0400

    Работа с рендером: более гибкая система задания количества проходов глубины резкости

--- layers/xrRenderPC_R2/r2_rendertarget_phase_depth_of_field.cpp
+++ layers/xrRenderPC_R2/r2_rendertarget_phase_depth_of_field.cpp
@@ -5,7 +5,7 @@
 #include "stdafx.h"
 #include "r2_rendertarget.h"
 ///////////////////////////////////////////////////////////////////////////////////
-void CRenderTarget::depth_of_field_pass_first()
+void CRenderTarget::calculate_depth_of_field()
 {
 	u_setrt(rt_GBuffer_Albedo, NULL, NULL, NULL);
 
@@ -57,118 +57,9 @@ void CRenderTarget::depth_of_field_pass_first()
 	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
 }
 
-void CRenderTarget::depth_of_field_pass_second()
-{
-	u_setrt(rt_GBuffer_Albedo, NULL, NULL, NULL);
-
-	RCache.set_CullMode(CULL_NONE);
-	RCache.set_Stencil(FALSE);
-
-	// Constants
-	u32 Offset = 0;
-	u32 C = color_rgba(0, 0, 0, 255);
-
-	float w = float(Device.dwWidth);
-	float h = float(Device.dwHeight);
-
-	float d_Z = EPS_S;
-	float d_W = 1.f;
-
-	Fvector2 p0, p1;
-	p0.set(0.5f / w, 0.5f / h);
-	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
-
-	// Fill vertex buffer
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0, h, d_Z, d_W, C, p0.x, p1.y);
-	pv++;
-	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y);
-	pv++;
-	pv->set(w, h, d_Z, d_W, C, p1.x, p1.y);
-	pv++;
-	pv->set(w, 0, d_Z, d_W, C, p1.x, p0.y);
-	pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
-
-	// Set pass
-	RCache.set_Element(s_dof->E[1]);
-
-	Fvector3 dof;
-	Fvector2 vDofKernel;
-	g_pGamePersistent->GetCurrentDof(dof);
-	vDofKernel.set(0.5f / Device.dwWidth, 0.5f / Device.dwHeight);
-	vDofKernel.mul(ps_r2_dof_kernel_size);
-
-	RCache.set_c("dof_params", dof.x, dof.y, dof.z, ps_r2_dof_sky);
-	RCache.set_c("dof_kernel", vDofKernel.x, vDofKernel.y, ps_r2_dof_kernel_size, 0);
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-}
-
-void CRenderTarget::depth_of_field_pass_third()
-{
-	u_setrt(rt_GBuffer_Albedo, NULL, NULL, NULL);
-
-	RCache.set_CullMode(CULL_NONE);
-	RCache.set_Stencil(FALSE);
-
-	// Constants
-	u32 Offset = 0;
-	u32 C = color_rgba(0, 0, 0, 255);
-
-	float w = float(Device.dwWidth);
-	float h = float(Device.dwHeight);
-
-	float d_Z = EPS_S;
-	float d_W = 1.f;
-
-	Fvector2 p0, p1;
-	p0.set(0.5f / w, 0.5f / h);
-	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
-
-	// Fill vertex buffer
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0, h, d_Z, d_W, C, p0.x, p1.y);
-	pv++;
-	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y);
-	pv++;
-	pv->set(w, h, d_Z, d_W, C, p1.x, p1.y);
-	pv++;
-	pv->set(w, 0, d_Z, d_W, C, p1.x, p0.y);
-	pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
-
-	// Set pass
-	RCache.set_Element(s_dof->E[2]);
-
-	Fvector3 dof;
-	Fvector2 vDofKernel;
-	g_pGamePersistent->GetCurrentDof(dof);
-	vDofKernel.set(0.5f / Device.dwWidth, 0.5f / Device.dwHeight);
-	vDofKernel.mul(ps_r2_dof_kernel_size);
-
-	RCache.set_c("dof_params", dof.x, dof.y, dof.z, ps_r2_dof_sky);
-	RCache.set_c("dof_kernel", vDofKernel.x, vDofKernel.y, ps_r2_dof_kernel_size, 0);
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-}
-
 void CRenderTarget::phase_depth_of_field()
 {
-	depth_of_field_pass_first();
-
-	if (ps_r2_dof_quality >= 2 && !ps_r2_ls_flags.test(R2FLAG_HARD_OPTIMIZATION))
-		depth_of_field_pass_second();
-
-	if (ps_r2_dof_quality >= 3 && !ps_r2_ls_flags.test(R2FLAG_HARD_OPTIMIZATION))
-		depth_of_field_pass_third();
+	for (int i = 0; i < ps_r2_dof_quality; i++)
+		calculate_depth_of_field();
 }
 ///////////////////////////////////////////////////////////////////////////////////
