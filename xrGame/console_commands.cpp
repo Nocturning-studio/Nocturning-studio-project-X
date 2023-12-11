5ff2577a21fa6d6c6657601b4c9652804c46b08d
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2023-12-11 19:16:06 +0400

    Перенос правок: Перенес g_spawn и g_spawn_to_inventory в целях отладки (Код взят от OGSR)

--- xrGame/console_commands.cpp
+++ xrGame/console_commands.cpp
@@ -1161,6 +1161,72 @@ struct CCC_JumpToLevel : public IConsole_Command
 		Msg("! There is no level \"%s\" in the game graph!", level);
 	}
 };
+
+class CCC_Spawn : public IConsole_Command
+{
+  public:
+	CCC_Spawn(LPCSTR N) : IConsole_Command(N)
+	{
+	}
+
+	void Execute(LPCSTR args)
+	{
+		if (!g_pGameLevel)
+			return;
+
+		if (!pSettings->section_exist(args))
+		{
+			Msg("! Can't find section: %s", args);
+			return;
+		}
+
+		if (auto tpGame = smart_cast<game_sv_Single*>(Level().Server->game))
+			tpGame->alife().spawn_item(args, Actor()->Position(), Actor()->ai_location().level_vertex_id(),
+									   Actor()->ai_location().game_vertex_id(), ALife::_OBJECT_ID(-1));
+	}
+};
+
+class CCC_SpawnToInventory : public IConsole_Command
+{
+  public:
+	CCC_SpawnToInventory(LPCSTR N) : IConsole_Command(N)
+	{
+	}
+
+	void Execute(LPCSTR args)
+	{
+		if (!g_pGameLevel)
+			return;
+
+		if (!pSettings->section_exist(args))
+		{
+			Msg("! Can't find section: %s", args);
+			return;
+		}
+
+		if (auto tpGame = smart_cast<game_sv_Single*>(Level().Server->game))
+		{
+			NET_Packet packet;
+			packet.w_begin(M_SPAWN);
+			packet.w_stringZ(args);
+
+			CSE_Abstract* item =
+				tpGame->alife().spawn_item(args, Actor()->Position(), Actor()->ai_location().level_vertex_id(),
+										   Actor()->ai_location().game_vertex_id(), 0, false);
+			item->Spawn_Write(packet, FALSE);
+			tpGame->alife().server().FreeID(item->ID, 0);
+			F_entity_Destroy(item);
+
+			ClientID clientID;
+			clientID.set(0xffff);
+
+			u16 dummy;
+			packet.r_begin(dummy);
+			VERIFY(dummy == M_SPAWN);
+			tpGame->alife().server().Process_spawn(packet, clientID);
+		}
+	}
+};
 #endif // MASTER_GOLD
 
 #include "GamePersistent.h"
@@ -1677,14 +1743,15 @@ void CCC_RegisterCommands()
 
 #ifndef MASTER_GOLD
 	CMD1(CCC_JumpToLevel, "jump_to_level");
+	CMD1(CCC_Spawn, "g_spawn");
+	CMD1(CCC_SpawnToInventory, "g_spawn_to_inventory");
 	CMD3(CCC_Mask, "g_god", &psActorFlags, AF_GODMODE);
 	CMD3(CCC_Mask, "g_unlimitedammo", &psActorFlags, AF_UNLIMITEDAMMO);
 	CMD1(CCC_Script, "run_script");
 	CMD1(CCC_ScriptCommand, "run_string");
 	CMD1(CCC_TimeFactor, "time_factor");
-#endif // MASTER_GOLD
-
 	CMD3(CCC_Mask, "g_autopickup", &psActorFlags, AF_AUTOPICKUP);
+#endif // MASTER_GOLD
 
 #ifdef DEBUG
 	CMD1(CCC_LuaHelp, "lua_help");
