9e4e8d770a0480ff54ffc320f7f507052321a14a
mihan-323 <81863440+mihan-323@users.noreply.github.com>
2024-02-26 23:03:04 +0400

    Рендер R2: Исправлены баги HBAO+, исправлено SSAO, переведено на 4х4 шум и bilinear denoise

--- layers/xrRenderPC_R2/Blender_ambient_occlusion.cpp
+++ layers/xrRenderPC_R2/Blender_ambient_occlusion.cpp
@@ -23,38 +23,28 @@ void CBlender_ambient_occlusion::Compile(CBlender_Compile& C)
 
 	switch (C.iElement)
 	{
-	case 0:
-		C.r_Pass("null", "ambient_occlusion_stage_pass_hbao", FALSE, FALSE, FALSE);
+	case SE_AO_SSAO:
+		// raw ao
+		C.r_Pass("null", "ambient_occlusion_stage_pass_ssao", FALSE, FALSE, FALSE);
 		C.r_Sampler_rtf("s_gbuffer_normal", r2_RT_GBuffer_Normal);
 		C.r_Sampler_rtf("s_gbuffer_position", r2_RT_GBuffer_Position);
-		jitter(C);
+		C.r_End();
+
+		// filer
+		C.r_Pass("null", "ambient_occlusion_blurring_stage_pass_bilinear_filter", FALSE, FALSE, FALSE);
+		C.r_Sampler_clf("s_ao", r2_RT_ao);
 		C.r_End();
 		break;
-	case 1:
+	case SE_AO_HBAO_PLUS:
+		// raw ao
 		C.r_Pass("null", "ambient_occlusion_stage_pass_hbao_plus", FALSE, FALSE, FALSE);
 		C.r_Sampler_rtf("s_gbuffer_normal", r2_RT_GBuffer_Normal);
 		C.r_Sampler_rtf("s_gbuffer_position", r2_RT_GBuffer_Position);
-		jitter(C);
 		C.r_End();
-		break;
-	case 2:
+
+		// filer
 		C.r_Pass("null", "ambient_occlusion_blurring_stage_pass_bilinear_filter", FALSE, FALSE, FALSE);
-		C.r_Sampler_rtf("s_ao", r2_RT_ao);
-		jitter(C);
-		C.r_End();
-		break;
-	case 3:
-		C.r_Pass("null", "ambient_occlusion_blurring_stage_pass_vertical_filter", FALSE, FALSE, FALSE);
-		C.r_Sampler_rtf("s_ao", r2_RT_ao);
-		C.r_Sampler_rtf("s_gbuffer_position", r2_RT_GBuffer_Position);
-		jitter(C);
-		C.r_End();
-		break;
-	case 4:
-		C.r_Pass("null", "ambient_occlusion_blurring_stage_pass_horizontal_filter", FALSE, FALSE, FALSE);
-		C.r_Sampler_rtf("s_ao", r2_RT_ao);
-		C.r_Sampler_rtf("s_gbuffer_position", r2_RT_GBuffer_Position);
-		jitter(C);
+		C.r_Sampler_clf("s_ao", r2_RT_ao);
 		C.r_End();
 		break;
 	}
--- layers/xrRenderPC_R2/r2_rendertarget_phase_ao.cpp
+++ layers/xrRenderPC_R2/r2_rendertarget_phase_ao.cpp
@@ -4,213 +4,57 @@
 ///////////////////////////////////////////////////////////////////////////////////
 #include "stdafx.h"
 #include "r2_rendertarget.h"
+#include "Blender_ambient_occlusion.h"
 ///////////////////////////////////////////////////////////////////////////////////
-void CRenderTarget::phase_create_ao()
-{
-	// Constants
-	u32 Offset = 0;
-	u32 C = color_rgba(0, 0, 0, 255);
-
-	float w = float(Device.dwWidth);
-	float h = float(Device.dwHeight);
-
-	float d_Z = EPS_S;
-	float d_W = 1.f;
-
-	Fvector2 p0, p1;
-	p0.set(0.5f / w, 0.5f / h);
-	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
-
-	// Set output RT
-	u_setrt(rt_ao, nullptr, nullptr, HW.pBaseZB);
-
-	RCache.set_CullMode(CULL_NONE);
-	RCache.set_Stencil(FALSE);
-
-	// Fill vertex buffer
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0, h, d_Z, d_W, C, p0.x, p1.y);
-	pv++;
-	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y);
-	pv++;
-	pv->set(w, h, d_Z, d_W, C, p1.x, p1.y);
-	pv++;
-	pv->set(w, 0, d_Z, d_W, C, p1.x, p0.y);
-	pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
-
-	// Set pass
-	switch (ps_r2_ao)
-	{
-	case 1: // HBAO
-		RCache.set_Element(s_ambient_occlusion->E[0]);
-		break;
-	case 2: // HBAO+
-		RCache.set_Element(s_ambient_occlusion->E[1]);
-		break;
-	}
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-}
-
-void CRenderTarget::billinear_filter()
-{
-	// Constants
-	u32 Offset = 0;
-	u32 C = color_rgba(0, 0, 0, 255);
-
-	float w = float(Device.dwWidth);
-	float h = float(Device.dwHeight);
-
-	float d_Z = EPS_S;
-	float d_W = 1.f;
-
-	Fvector2 p0, p1;
-	p0.set(0.5f / w, 0.5f / h);
-	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
-
-	// Set output RT
-	u_setrt(rt_ao, nullptr, nullptr, HW.pBaseZB);
-
-	RCache.set_CullMode(CULL_NONE);
-	RCache.set_Stencil(FALSE);
-
-	// Fill vertex buffer
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0, h, d_Z, d_W, C, p0.x, p1.y);
-	pv++;
-	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y);
-	pv++;
-	pv->set(w, h, d_Z, d_W, C, p1.x, p1.y);
-	pv++;
-	pv->set(w, 0, d_Z, d_W, C, p1.x, p0.y);
-	pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
-
-	// Set pass
-	RCache.set_Element(s_ambient_occlusion->E[2]);
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-}
-
-void CRenderTarget::vertical_filter()
-{
-	// Constants
-	u32 Offset = 0;
-	u32 C = color_rgba(0, 0, 0, 255);
-
-	float w = float(Device.dwWidth);
-	float h = float(Device.dwHeight);
-
-	float d_Z = EPS_S;
-	float d_W = 1.f;
-
-	Fvector2 p0, p1;
-	p0.set(0.5f / w, 0.5f / h);
-	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
-
-	// Set output RT
-	u_setrt(rt_ao, nullptr, nullptr, HW.pBaseZB);
-
-	RCache.set_CullMode(CULL_NONE);
-	RCache.set_Stencil(FALSE);
-
-	// Fill vertex buffer
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0, h, d_Z, d_W, C, p0.x, p1.y);
-	pv++;
-	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y);
-	pv++;
-	pv->set(w, h, d_Z, d_W, C, p1.x, p1.y);
-	pv++;
-	pv->set(w, 0, d_Z, d_W, C, p1.x, p0.y);
-	pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
-
-	// Set pass
-	RCache.set_Element(s_ambient_occlusion->E[3]);
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-}
-
-void CRenderTarget::horizontal_filter()
-{
-	// Constants
-	u32 Offset = 0;
-	u32 C = color_rgba(0, 0, 0, 255);
-
-	float w = float(Device.dwWidth);
-	float h = float(Device.dwHeight);
-
-	float d_Z = EPS_S;
-	float d_W = 1.f;
-
-	Fvector2 p0, p1;
-	p0.set(0.5f / w, 0.5f / h);
-	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
-
-	// Set output RT
-	u_setrt(rt_ao, nullptr, nullptr, HW.pBaseZB);
-
-	RCache.set_CullMode(CULL_NONE);
-	RCache.set_Stencil(FALSE);
-
-	// Fill vertex buffer
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0, h, d_Z, d_W, C, p0.x, p1.y);
-	pv++;
-	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y);
-	pv++;
-	pv->set(w, h, d_Z, d_W, C, p1.x, p1.y);
-	pv++;
-	pv->set(w, 0, d_Z, d_W, C, p1.x, p0.y);
-	pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
-
-	// Set pass
-	RCache.set_Element(s_ambient_occlusion->E[4]);
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-}
-
-void CRenderTarget::depth_based_filtering()
-{
-	vertical_filter();
-	horizontal_filter();
-}
-
-void CRenderTarget::phase_filtering()
-{
-	switch (ps_r2_ao)
-	{
-	case 1: // HBAO
-		depth_based_filtering();
-		break;
-	case 2: // HBAO+
-		billinear_filter();
-		break;
-	}
-}
-
 void CRenderTarget::phase_ao()
 {
-	phase_create_ao();
-	phase_filtering();
+	u32 Offset = 0;
+
+	float w = float(Device.dwWidth);
+	float h = float(Device.dwHeight);
+
+	float d_Z = EPS_S;
+	float d_W = 1.f;
+
+	Fvector2 p0, p1;
+	p0.set(0.5f / w, 0.5f / h);
+	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
+
+	RCache.set_CullMode(CULL_NONE);
+	RCache.set_Stencil(FALSE);
+
+	// Fill vertex buffer
+	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
+	pv->set(0, h, 0, 0, 0, p0.x, p1.y);
+	pv++;
+	pv->set(0, 0, 0, 0, 0, p0.x, p0.y);
+	pv++;
+	pv->set(w, h, 0, 0, 0, p1.x, p1.y);
+	pv++;
+	pv->set(w, 0, 0, 0, 0, p1.x, p0.y);
+	pv++;
+	RCache.Vertex.Unlock(4, g_combine->vb_stride);
+
+	RCache.set_Geometry(g_combine);
+
+	u_setrt(rt_ao, NULL, NULL, NULL);
+
+	// switch ssao mode
+	if (ps_r2_ao == 1)
+	{
+		for (u32 i = 0; i < s_ambient_occlusion->E[SE_AO_SSAO]->passes.size(); i++)
+		{
+			RCache.set_Element(s_ambient_occlusion->E[SE_AO_SSAO], i);
+			RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
+		}
+	}
+	else if (ps_r2_ao == 2)
+	{
+		for (u32 i = 0; i < s_ambient_occlusion->E[SE_AO_HBAO_PLUS]->passes.size(); i++)
+		{
+			RCache.set_Element(s_ambient_occlusion->E[SE_AO_HBAO_PLUS], i);
+			RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
+		}
+	}
 }
 ///////////////////////////////////////////////////////////////////////////////////
