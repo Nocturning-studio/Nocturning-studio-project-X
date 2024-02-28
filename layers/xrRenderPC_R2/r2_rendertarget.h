d0e24654c421648296c3b23cd5877b357f3b04b0
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-02-28 20:20:58 +0400

    Рефакторинг кода глубины резкости

--- layers/xrRenderPC_R2/r2_rendertarget.h
+++ layers/xrRenderPC_R2/r2_rendertarget.h
@@ -262,9 +262,7 @@ class CRenderTarget : public IRender_Target
 	void phase_distortion();
 
 	void depth_of_field_pass_poisson_filter();
-	void depth_of_field_pass_vertical();
-	void depth_of_field_pass_horizontal();
-	void calculate_depth_of_field();
+	void depth_of_field_pass_separated_filter();
 	void phase_depth_of_field();
 
 	void phase_barrel_blur();
--- layers/xrRenderPC_R2/r2_rendertarget_phase_depth_of_field.cpp
+++ layers/xrRenderPC_R2/r2_rendertarget_phase_depth_of_field.cpp
@@ -57,7 +57,7 @@ void CRenderTarget::depth_of_field_pass_poisson_filter()
 	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
 }
 
-void CRenderTarget::depth_of_field_pass_vertical()
+void CRenderTarget::depth_of_field_pass_separated_filter()
 {
 	u_setrt(rt_Generic_0, NULL, NULL, NULL);
 
@@ -90,8 +90,8 @@ void CRenderTarget::depth_of_field_pass_vertical()
 	pv++;
 	RCache.Vertex.Unlock(4, g_combine->vb_stride);
 
-	// Set pass
-	RCache.set_Element(s_dof->E[1]);
+	// Set geometry
+	RCache.set_Geometry(g_combine);
 
 	Fvector3 dof;
 	Fvector2 vDofKernel;
@@ -99,66 +99,13 @@ void CRenderTarget::depth_of_field_pass_vertical()
 	vDofKernel.set(0.5f / Device.dwWidth, 0.5f / Device.dwHeight);
 	vDofKernel.mul(ps_r2_dof_kernel_size);
 
-	RCache.set_c("dof_params", dof.x, dof.y, dof.z, ps_r2_dof_sky);
-	RCache.set_c("dof_kernel", vDofKernel.x, vDofKernel.y, ps_r2_dof_kernel_size, 0);
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
-}
-
-void CRenderTarget::depth_of_field_pass_horizontal()
-{
-	u_setrt(rt_Generic_0, NULL, NULL, NULL);
-
-	RCache.set_CullMode(CULL_NONE);
-	RCache.set_Stencil(FALSE);
-
-	// Constants
-	u32 Offset = 0;
-	u32 C = color_rgba(0, 0, 0, 255);
-
-	float w = float(Device.dwWidth);
-	float h = float(Device.dwHeight);
-
-	float d_Z = EPS_S;
-	float d_W = 1.f;
-
-	Fvector2 p0, p1;
-	p0.set(0.5f / w, 0.5f / h);
-	p1.set((w + 0.5f) / w, (h + 0.5f) / h);
-
-	// Fill vertex buffer
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0, h, d_Z, d_W, C, p0.x, p1.y);
-	pv++;
-	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y);
-	pv++;
-	pv->set(w, h, d_Z, d_W, C, p1.x, p1.y);
-	pv++;
-	pv->set(w, 0, d_Z, d_W, C, p1.x, p0.y);
-	pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
-
-	// Set pass
-	RCache.set_Element(s_dof->E[2]);
-
-	Fvector3 dof;
-	Fvector2 vDofKernel;
-	g_pGamePersistent->GetCurrentDof(dof);
-	vDofKernel.set(0.5f / Device.dwWidth, 0.5f / Device.dwHeight);
-	vDofKernel.mul(ps_r2_dof_kernel_size);
-
-	RCache.set_c("dof_params", dof.x, dof.y, dof.z, ps_r2_dof_sky);
-	RCache.set_c("dof_kernel", vDofKernel.x, vDofKernel.y, ps_r2_dof_kernel_size, 0);
-
-	// Set geometry
-	RCache.set_Geometry(g_combine);
-
-	// Draw
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
+	for (u32 i = 0; i < s_dof->E[1]->passes.size(); i++)
+	{
+		RCache.set_Element(s_dof->E[1], i);
+		RCache.set_c("dof_params", dof.x, dof.y, dof.z, ps_r2_dof_sky);
+		RCache.set_c("dof_kernel", vDofKernel.x, vDofKernel.y, ps_r2_dof_kernel_size, 0);
+		RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
+	}
 }
 
 void CRenderTarget::phase_depth_of_field()
@@ -171,8 +118,7 @@ void CRenderTarget::phase_depth_of_field()
 	{
 		for (int i = 0; i < 2; i++)
 		{
-			depth_of_field_pass_vertical();
-			depth_of_field_pass_horizontal();
+			depth_of_field_pass_separated_filter();
 		}
 	}
 }
