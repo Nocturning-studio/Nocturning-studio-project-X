70a69930bb821c91b7352cc79c74e3a8f7778a6d
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-03-13 14:34:20 +0400

    Переработка работы с материалами

--- layers/xrRender/shader_name_generator.cpp
+++ layers/xrRender/shader_name_generator.cpp
@@ -51,11 +51,12 @@ void generate_shader_name(CBlender_Compile& C, bool bIsHightQualityGeometry, LPC
 	}
 	else
 	{
+		//If bump not connected - try find unconnected texture with similar name
 		strcpy_s(BumpTexture, sizeof(BumpTexture), AlbedoTexture);
 		strconcat(sizeof(BumpTexture), BumpTexture, BumpTexture, "_bump");
 
-		if (!FS.exist(Dummy, "$game_textures$", BumpTexture, ".dds"))
-			strcpy_s(BumpTexture, sizeof(BumpTexture), "ed\\ed_dummy_bump");
+		if (FS.exist(Dummy, "$game_textures$", BumpTexture, ".dds"))
+			bUseBump = true;
 	}
 
 	// Get bump decompression map
@@ -66,45 +67,49 @@ void generate_shader_name(CBlender_Compile& C, bool bIsHightQualityGeometry, LPC
 	bool bDetailTextureExist = C.detail_texture;
 	bool bDetailTextureAllowed = C.bDetail;
 	bool bUseDetail = bDetailTextureExist && bDetailTextureAllowed;
+	bool bUseDetailBump = false;
 
 	// Get  for base texture for material
 	strcpy_s(AlbedoTexture, sizeof(AlbedoTexture), *C.L_textures[0]);
 
 	// Get detail texture
 	if (bUseDetail)
+	{
 		strcpy_s(DetailAlbedoTexture, sizeof(DetailAlbedoTexture), C.detail_texture);
-	else
-		strcpy_s(DetailAlbedoTexture, sizeof(DetailAlbedoTexture), "ed\\ed_dummy_detail");
 
-	// Get bump for detail texture
-	strcpy_s(DetailBumpTexture, sizeof(DetailBumpTexture), DetailAlbedoTexture);
+		// Get bump for detail texture
+		strcpy_s(DetailBumpTexture, sizeof(DetailBumpTexture), DetailAlbedoTexture);
 
-	// Checking for existance
-	if (FS.exist(Dummy, "$game_textures$", DetailBumpTexture, ".dds"))
-		strconcat(sizeof(DetailBumpTexture), DetailBumpTexture, DetailBumpTexture, "_bump");
-	else
-		strcpy_s(DetailBumpTexture, sizeof(DetailBumpTexture), "ed\\ed_dummy_bump");
+		// Checking for existance
+		if (FS.exist(Dummy, "$game_textures$", DetailBumpTexture, ".dds"))
+		{
+			bUseDetailBump = true;
 
-	// Get bump decompression map for detail texture
-	strcpy_s(DetailBumpCorrectionTexture, sizeof(DetailBumpCorrectionTexture), DetailBumpTexture);
-	strconcat(sizeof(DetailBumpCorrectionTexture), DetailBumpCorrectionTexture, DetailBumpCorrectionTexture, "#");
+			strconcat(sizeof(DetailBumpTexture), DetailBumpTexture, DetailBumpTexture, "_bump");
+
+			// Get bump decompression map for detail texture
+			strcpy_s(DetailBumpCorrectionTexture, sizeof(DetailBumpCorrectionTexture), DetailBumpTexture);
+			strconcat(sizeof(DetailBumpCorrectionTexture), DetailBumpCorrectionTexture, DetailBumpCorrectionTexture,"#");
+		}
+	}
 
 	// Check lightmap existing
-	bool bUseLightMap;
-	if (C.L_textures.size() < 3)
-	{
-		bUseLightMap = false;
-	}
-	else
+	bool bUseLightMap = false;
+	if (C.L_textures.size() >= 3)
 	{
 		pcstr HemisphereLightMapTextureName = C.L_textures[2].c_str();
 		pcstr LightMapTextureName = C.L_textures[1].c_str();
+
 		if ((HemisphereLightMapTextureName[0] == 'l' && HemisphereLightMapTextureName[1] == 'm' &&
 			 HemisphereLightMapTextureName[2] == 'a' && HemisphereLightMapTextureName[3] == 'p') &&
 			(LightMapTextureName[0] == 'l' && LightMapTextureName[1] == 'm' &&
 			 LightMapTextureName[2] == 'a' && LightMapTextureName[3] == 'p'))
 		{
 			bUseLightMap = true;
+
+			// Get LightMap texture
+			strcpy_s(HemisphereLightMapTexture, sizeof(HemisphereLightMapTexture), *C.L_textures[2]);
+			strcpy_s(LightMapTexture, sizeof(LightMapTexture), *C.L_textures[1]);
 		}
 		else
 		{
@@ -112,18 +117,12 @@ void generate_shader_name(CBlender_Compile& C, bool bIsHightQualityGeometry, LPC
 		}
 	}
 
-	// Get LightMap texture
-	if (bUseLightMap)
-	{
-		strcpy_s(HemisphereLightMapTexture, sizeof(HemisphereLightMapTexture), *C.L_textures[2]);
-		strcpy_s(LightMapTexture, sizeof(LightMapTexture), *C.L_textures[1]);
-	}
-
 	// Get BakedAO texture
+	bool bUseBakedAO = false;
 	strcpy_s(BakedAOTexture, sizeof(BakedAOTexture), AlbedoTexture);
 	strconcat(sizeof(BakedAOTexture), BakedAOTexture, BakedAOTexture, "_ao");
-	if (!FS.exist(Dummy, "$game_textures$", BakedAOTexture, ".dds"))
-		strcpy_s(BakedAOTexture, sizeof(BakedAOTexture), "ed\\debug_white");
+	if (FS.exist(Dummy, "$game_textures$", BakedAOTexture, ".dds"))
+		bUseBakedAO = true;
 
 	// Starting generate shader name
 	strconcat(sizeof(NewPixelShaderName), NewPixelShaderName, "gbuffer_stage_", PixelShaderName);
@@ -134,6 +133,8 @@ void generate_shader_name(CBlender_Compile& C, bool bIsHightQualityGeometry, LPC
 
 	// Create shader with alpha testing if need
 	C.sh_macro(bUseAlpha, "USE_ALPHA_TEST", "1");
+
+	C.sh_macro(bUseBump, "USE_BUMP", "1");
 
 	// Create shader with normal mapping or displacement if need
 	int BumpType = 0;
@@ -156,36 +157,43 @@ void generate_shader_name(CBlender_Compile& C, bool bIsHightQualityGeometry, LPC
 	// Create shader with deatil texture if need
 	C.sh_macro(bUseDetail, "USE_TDETAIL", "1");
 
+	C.sh_macro(bUseDetailBump, "USE_DETAIL_BUMP", "1");
+
+	C.sh_macro(bUseBakedAO, "USE_BAKED_AO", "1");
+
 	// Create shader pass
 	C.r_Pass(NewVertexShaderName, NewPixelShaderName, FALSE);
 
-	C.r_Sampler("s_base", AlbedoTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_POINT,
-				D3DTEXF_ANISOTROPIC);
+	C.r_Sampler("s_base", AlbedoTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_POINT, D3DTEXF_ANISOTROPIC);
+	
+	if (bUseBakedAO)
+	{
+		C.r_Sampler("s_baked_ao", BakedAOTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR, D3DTEXF_ANISOTROPIC);
+	}
 
-	C.r_Sampler("s_baked_ao", BakedAOTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR,
-				D3DTEXF_ANISOTROPIC);
+	if (bUseBump)
+	{
+		C.r_Sampler("s_bumpX", BumpCorrectionTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR, D3DTEXF_ANISOTROPIC);
 
-	C.r_Sampler("s_bumpX", BumpCorrectionTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR,
-				D3DTEXF_ANISOTROPIC);
+		C.r_Sampler("s_bump", BumpTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR, D3DTEXF_ANISOTROPIC);
+	}
 
-	C.r_Sampler("s_bump", BumpTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR,
-				D3DTEXF_ANISOTROPIC);
+	if (bUseDetail)
+	{
+		C.r_Sampler("s_detail", DetailAlbedoTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR, D3DTEXF_ANISOTROPIC);
 
-	C.r_Sampler("s_detail", DetailAlbedoTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR,
-				D3DTEXF_ANISOTROPIC);
+		if (bUseDetailBump)
+		{
+			C.r_Sampler("s_detailBump", DetailBumpTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR, D3DTEXF_ANISOTROPIC);
 
-	C.r_Sampler("s_detailBump", DetailBumpTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR,
-				D3DTEXF_ANISOTROPIC);
-
-	C.r_Sampler("s_detailBumpX", DetailBumpCorrectionTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC,
-				D3DTEXF_LINEAR, D3DTEXF_ANISOTROPIC);
+			C.r_Sampler("s_detailBumpX", DetailBumpCorrectionTexture, false, D3DTADDRESS_WRAP, D3DTEXF_ANISOTROPIC, D3DTEXF_LINEAR, D3DTEXF_ANISOTROPIC);
+		}
+	}
 
 	if (bUseLightMap)
 	{
-		C.r_Sampler("s_hemi", HemisphereLightMapTexture, false, D3DTADDRESS_CLAMP, D3DTEXF_GAUSSIANQUAD, D3DTEXF_NONE,
-					D3DTEXF_LINEAR);
-		C.r_Sampler("s_lmap", LightMapTexture, false, D3DTADDRESS_CLAMP, D3DTEXF_GAUSSIANQUAD, D3DTEXF_NONE,
-					D3DTEXF_LINEAR);
+		C.r_Sampler("s_hemi", HemisphereLightMapTexture, false, D3DTADDRESS_CLAMP, D3DTEXF_GAUSSIANQUAD, D3DTEXF_NONE, D3DTEXF_LINEAR);
+		C.r_Sampler("s_lmap", LightMapTexture, false, D3DTADDRESS_CLAMP, D3DTEXF_GAUSSIANQUAD, D3DTEXF_NONE, D3DTEXF_LINEAR);
 	}
 
 	jitter(C);
